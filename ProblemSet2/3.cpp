1325d3f3550f884982793d8ebc84d096bcb830ab
Maycol <42551238+Sorbete-Sempai123@users.noreply.github.com>
2019-04-11 14:43:50 -0500

    complete c:

--- ProblemSet2/3.cpp
+++ ProblemSet2/3.cpp
@@ -6,7 +6,6 @@ int main(){      //to fix this code , move the function printNum
     printNum(35);//over the main function or do a prototype of
     return 0;    //the function :v
 }
-
 void printNum(int number){ std::cout << number; }
 */
 //--------------------------------------------------------------
@@ -14,7 +13,6 @@ void printNum(int number){ std::cout << number; }
 void printNum (){       //to fix this code , the function should 
     std::cout << number;//have the parameter of the variable number
 };                      //2.- var should be a global variable
-
 int main(){
     int number = 35;
     printNum(number);
@@ -36,13 +34,38 @@ int main(){           //paso por referencia
 /*  3.4
 int difference(const int x,const int y){
     int diff = abs( x - y);//abs(n) returns absolute value of n
-    //to fix the code , need return variable diff;
+    						//to fix the code , need return 
+							//variable diff;
 }
 int main(){
     std::cout<<difference (24,1238);
     return 0;
 }
 */
+//---------------------------------------------------------------
+/*  3.5
+int sum ( const int x, const int y ) {
+return x + y ;
+}
 
+int main () {
+std :: cout << sum (1, 2, 3); // Should print 6
+return 0;					  //to fix this code , add an argument or do a
+ }						      //sum(sum(1,2),3)
+*/
+//---------------------------------------------------------------
+/*  3.6
+const int ARRAY_LEN = 10;
+
+int main () {
+    int arr [ ARRAY_LEN ] = {10}; // Note implicit initialization of
+                                  // other elements
+    int * xPtr = arr , yPtr = arr + ARRAY_LEN - 1;
+    std :: cout << * xPtr << ' ' << *yPtr ; // Should output 10 0
+    return 0;								 //to fix this code , add a pointer to 
+ }											 //the variable yPtr or add a pointer to
+											 // "arr + ARRAY_LEN - 1" and delete the
+											 //pointer of yPtr in the cout
+*/
 
 
