5cf560afe62365d9849a1ce2ae0b40a9d1d89cec
Maycol <42551238+Sorbete-Sempai123@users.noreply.github.com>
2019-04-17 08:49:11 -0500

    qweasd

---
+++ ProjectEuler/18.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int maximum_path_sum(int **arr,int size){
+	int aux=arr[0][0];
+	for(int i=1;i<size;i++){
+		
+	}
+}
+
+int main(){
+	int tam;
+	cin>>tam;
+	int arr[tam*(tam+1)/2];
+	for(int i=0;i<tam*(tam+1)/2;i++){
+		cin>>arr[i];
+	}
+	for(int i=0;i<tam*(tam+1)/2;i++)cout<<arr[i]<<endl;
+}
